759e78e46b5c8c00d676b30565f1881010a46cf2
MarileneCorrea <maridsccorrea@gmail.com>
2023-01-02 22:32:25 -0300

    header pronto

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,14 @@
 import Link from "../Link";
 import "./style.css";
+import Logo from "../../assets/image/logo.png";
 
 function Header() {
     return (
         <header>
-            <div>
                 <nav>
+                    <img src={Logo} alt="Logo Bergamotta" />
                     <Link texto="Login / Cadastro" redirect="/login" />
                 </nav>
-                <img src=""></img>
-            </div>
         </header>
     );
 }
